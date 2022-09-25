d2dd4da558f3c0aeecd4d797d1f87ca0323b59b9
Brian Grant <brian@skeletonjack.net>
2022-09-25 08:31:37 -0500

    author

--- keyboards/cocot46plus/keymaps/btgrant-76/keymap.c
+++ keyboards/cocot46plus/keymaps/btgrant-76/keymap.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2022 aki27
+Copyright 2022 @btgrant-76
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
