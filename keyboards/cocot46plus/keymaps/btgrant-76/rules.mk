b038d5880b126efec327f158a8944f801d1c7615
Brian Grant <brian@skeletonjack.net>
2022-11-06 14:26:17 -0600

    Enable RGB; disable VIA

--- keyboards/cocot46plus/keymaps/btgrant-76/rules.mk
+++ keyboards/cocot46plus/keymaps/btgrant-76/rules.mk
@@ -1,7 +1,5 @@
-VIA_ENABLE = yes # 2,356
+VIA_ENABLE = no # 2,356
 TAP_DANCE_ENABLE = yes # 1,044
-
-RGBLIGHT_ENABLE = no  # saving space while porting Corne Tap Dance and macros
 
 # space-saving
 MOUSEKEY_ENABLE = no  # 1,150
