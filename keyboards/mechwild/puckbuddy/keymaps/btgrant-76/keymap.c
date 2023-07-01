fc08d03c5596913551cef2759949e1c460ed89f4
Brian Grant <brian@skeletonjack.net>
2023-07-01 07:58:52 -0500

    WIP

--- keyboards/mechwild/puckbuddy/keymaps/btgrant-76/keymap.c
+++ keyboards/mechwild/puckbuddy/keymaps/btgrant-76/keymap.c
@@ -36,33 +36,67 @@ enum layer_names {
  *
  */
 
+/* TODO add buttons from the Logitech mouse
+ * [x]  media pause/play
+ * [ ]  desktop left/right
+ * [x]  all windows
+ * [x]  all application windows
+ */
+
+// TODO integrate with userspace
+// shortcuts
+#define BACK LCMD(KC_LBRC)
+#define FWD LCMD(KC_RBRC)
+#define MISS_CTL LCTL(KC_UP)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-    KC_MUTE,  MO(_FN3), MO(_FN2), MO(_FN1),       LGUI(KC_D),
-    MO(_FN2),                                     KC_HOME,
-    MO(_FN3),                                     KC_END,
-    KC_BTN3,                                      DPI_FINE,
-              KC_BTN1, KC_BTN2, KC_BTN2, KC_BTN1
+        KC_MUTE,  XXXXXXX, XXXXXXX, MO(_FN1),       KC_MPLY,
+        BACK,                                     FWD,
+        KC_WH_D,                                     MISS_CTL,
+        KC_WH_U,                                      DPI_FINE,
+                  KC_BTN1, KC_BTN2, KC_BTN3, LCTL(KC_DOWN)
     ),
     [_FN1] = LAYOUT(
-    RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS,          TAP_TOG,
-    DPI_UP,                                      TAP_UP,
-    DPI_DN,                                      TAP_DN,
-    KC_TRNS,                                     KC_TRNS,
-             KC_HOME, KC_PGUP, KC_PGDN, KC_END
+        RGB_TOG, XXXXXXX, XXXXXXX,  XXXXXXX,         TAP_TOG,
+        DPI_UP,                                      TAP_UP,
+        DPI_DN,                                      TAP_DN,
+        MO(_FN2),                                     KC_TRNS,
+                 KC_HOME, KC_PGUP, KC_PGDN, KC_END
     ),
     [_FN2] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_TOG,
-    KC_TRNS,                                     RGB_MOD,
-    KC_TRNS,                                     RGB_RMOD,
-    KC_TRNS,                                     KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, XXXXXXX, XXXXXXX, KC_TRNS,          RGB_TOG,
+        KC_TRNS,                                     RGB_MOD,
+        KC_TRNS,                                     RGB_RMOD,
+        KC_TRNS,                                     KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_FN3] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          QK_BOOT,
-    KC_TRNS,                                     KC_TRNS,
-    KC_TRNS,                                     KC_TRNS,
-    KC_TRNS,                                     KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, XXXXXXX, XXXXXXX, KC_TRNS,          QK_BOOT,
+        KC_TRNS,                                     KC_TRNS,
+        KC_TRNS,                                     KC_TRNS,
+        KC_TRNS,                                     KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+#ifdef ENCODER_MAP_ENABLE
+    bool encoder_update_user(uint8_t index, bool clockwise) {
+        return false;
+    };
+
+    const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+        [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+        [_FN1]  = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+        [_FN2]  = { ENCODER_CCW_CW(KC_MNXT, KC_MPRV), ENCODER_CCW_CW(KC_MNXT, KC_MPRV) },
+        [_FN3]  = { ENCODER_CCW_CW(FWD, BACK), ENCODER_CCW_CW(FWD, BACK) },
+    };
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
+}
+#endif
