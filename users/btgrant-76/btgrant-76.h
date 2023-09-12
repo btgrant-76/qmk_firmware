550a2c5f1e9abf9ebce3814e839bc91c510b1ef4
Brian Grant <brian@skeletonjack.net>
2023-09-12 04:34:14 -0500

    add combo definitions to userspace .h file

--- users/btgrant-76/btgrant-76.h
+++ users/btgrant-76/btgrant-76.h
@@ -42,6 +42,15 @@ enum custom_keycodes {
     NEW_SAFE_RANGE
 };
 
+#ifdef COMBO_ENABLE
+const uint16_t PROGMEM test_combo1[] = {KC_A, KC_B, COMBO_END};
+const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(test_combo1, KC_ESC),
+    COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+};
+#endif
+
 // Tap Dance declarations
 enum {
     TD_F1,
