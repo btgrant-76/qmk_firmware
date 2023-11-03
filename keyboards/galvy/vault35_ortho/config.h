7514a3c767a8561de1903d3553dbc19df3859f85
Brian Grant <brian@skeletonjack.net>
2023-11-02 21:06:55 -0500

    dakfu's Vial source by way of HaiZeus

---
+++ keyboards/galvy/vault35_ortho/config.h
@@ -0,0 +1,49 @@
+// Copyright 2022 haizeus (@haizeus)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x0411
+#define PRODUCT_ID 0xD106
+#define DEVICE_VER 0x0001
+#define MANUFACTURER galvy
+#define PRODUCT Vault35Ortho
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+#define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+
+/* Define encoder pads */
+#define ENCODERS_PAD_A { B5, D1 }
+//#define ENCODERS_PAD_B { B6, D0 }
+
+/* Encoder resolution */
+#define ENCODER_RESOLUTION 4
+#define TAP_CODE_DELAY 10
+
+/* Set 0 if debouncing isn't needed */
+#define Debounce 0
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Bootmagic Lite key configuration */
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
